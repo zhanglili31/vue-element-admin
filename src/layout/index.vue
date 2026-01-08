a6bebbe5e9dc626974ee8d407220cef75db6ba27
zhanglili <1137596624@qq.com>
2026-01-08 22:04:23 +0800

    恢复原布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <div :class="classObj" class="app-wrapper">
+  <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
     <div :class="{hasTagsView:needTagsView}" class="main-container">
@@ -12,10 +12,6 @@
         <settings />
       </right-panel>
     </div>
-  </div> -->
-
-  <div :class="classObj" class="app-wrapper">
-    <app-main />
   </div>
 </template>
 
